8892b3fe72b2c16939f366a63fc44f6c6e59dad2
Kislitsina-Maria <ma.kis@mail.ru>
2022-06-16 10:35:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 1450; //Стоимость билета
+        int miles = amount / 20; //Расчет миль
+        System.out.println("Количество миль: " + miles);
+
+    }
+}
